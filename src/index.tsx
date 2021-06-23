c44c5f003e60db081ba3c24036f75593220e80af
jaywcjlove <398188662@qq.com>
2021-06-24 04:09:11 +0800

    fix: fix html scripts errors.

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ export default React.forwardRef<HTMLIFrameElement, RunWebProps>((props, ref) => 
   const srcDoc = useMemo(() => {
     const jsString = js ? `<script type="text/javascript">${js}</script>` : '';
     const cssString = css ? `<style>${css}</style>` : '';
-    return `<!DOCTYPE html><html><head>${cssString}</head><body>${html}${jsString}</body></html>`;
+    return `<!DOCTYPE html><html><head>${cssString}</head><body>${html}</body>${jsString}</html>`;
   }, [css, html, js]);
 
   return <iframe title={title} width="100%" height="100%" style={{ border: 0 }} {...other} ref={ref} srcDoc={srcDoc} />;
