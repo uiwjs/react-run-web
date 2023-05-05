5cfcb94e10a3d79f90f669e980d564b070f2dc80
jaywcjlove <398188662@qq.com>
2023-05-05 09:38:09 +0800

    test: update test case.

--- src/__test__/index.test.tsx
+++ src/__test__/index.test.tsx
@@ -5,13 +5,14 @@ import RunWeb from '../';
 it('Should output a RunWeb', async () => {
   const component = TestRenderer.create(<RunWeb />);
   let tree = component.toJSON();
+  console.log('tree:', tree);
   if (tree && !Array.isArray(tree)) {
     expect(tree.type).toEqual('iframe');
     expect(tree.props.title).toEqual('Demo Title');
     expect(tree.props.width).toEqual('100%');
     expect(tree.props.height).toEqual('100%');
     expect(tree.props.style).toEqual({ border: 0 });
-    expect(tree.props.srcDoc).toEqual('<!DOCTYPE html><html><head></head><body></body></html>');
+    expect(tree.props.srcDoc).toBeUndefined();
   }
 });
 
@@ -24,9 +25,7 @@ it('RunWeb Props js="..."', async () => {
     expect(tree.props.width).toEqual('100%');
     expect(tree.props.height).toEqual('100%');
     expect(tree.props.style).toEqual({ border: 0 });
-    expect(tree.props.srcDoc).toEqual(
-      `<!DOCTYPE html><html><head></head><body></body><script type="text/javascript">console.log('hello world!')</script></html>`,
-    );
+    expect(tree.props.srcDoc).toBeUndefined();
   }
 });
 
@@ -39,8 +38,6 @@ it('RunWeb Props css="..."', async () => {
     expect(tree.props.width).toEqual('100%');
     expect(tree.props.height).toEqual('100%');
     expect(tree.props.style).toEqual({ border: 0 });
-    expect(tree.props.srcDoc).toEqual(
-      `<!DOCTYPE html><html><head><style>body { color:red; }</style></head><body></body></html>`,
-    );
+    expect(tree.props.srcDoc).toBeUndefined();
   }
 });
